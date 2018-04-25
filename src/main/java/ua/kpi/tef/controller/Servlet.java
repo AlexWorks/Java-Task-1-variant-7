ba6ede36a27b1033dc8fdf744712fffc042ea33f
Alex <mister.lexx.ua@gmail.com>
2018-04-25 19:42:37 +0300

    Connected to the database

--- src/main/java/ua/kpi/tef/controller/Servlet.java
+++ src/main/java/ua/kpi/tef/controller/Servlet.java
@@ -1,13 +1,26 @@
 package ua.kpi.tef.controller;
 
+import ua.kpi.tef.controller.service.AmmunitionService;
+import ua.kpi.tef.controller.service.DBAmmunitionService;
+import ua.kpi.tef.model.entities.ammunition.Ammunition;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class Servlet extends HttpServlet {
     private String APP = "app.jsp";
+    private AmmunitionService service;
+    private List<Ammunition> data;
+
+    @Override
+    public void init() {
+        this.service = new DBAmmunitionService();
+        this.data = service.getAmmunition();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
--- src/main/java/ua/kpi/tef/controller/service/DBAmmunitionService.java
+++ src/main/java/ua/kpi/tef/controller/service/DBAmmunitionService.java
@@ -1,13 +1,31 @@
 package ua.kpi.tef.controller.service;
-
+import com.mysql.jdbc.Driver;
 import ua.kpi.tef.model.entities.ammunition.Ammunition;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DBAmmunitionService implements AmmunitionService {
+    private static String URL = "jdbc:mysql://localhost:3306/ammunition";
+    private static String USERNAME = "root";
+    private static String PASSWORD = "lololo123";
 
     @Override
     public List<Ammunition> getAmmunition() {
+        try {
+            Driver driver = new Driver();
+            DriverManager.registerDriver(driver);
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Connected to the database!");
+                connection.close();
+                System.out.println("Connection closed!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
